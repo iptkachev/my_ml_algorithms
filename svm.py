89b9c32f1933284b83c265229b59bd5388cde693
Ilya Tkachev <skaatt@mail.ru>
2019-11-24 23:34:00 +0300

    feature: b calculates as mean of support vectors

--- svm.py
+++ svm.py
@@ -14,6 +14,11 @@ class MySVC(BaseEstimator):
         self.w = None
         self.b = None
 
+    def _isinstance_check(self, array):
+        if not isinstance(array, np.ndarray):
+            array = np.asarray(array)
+        return array
+
     def _lagrange_func(self, X, y, alphas):
         return - (alphas.sum() - 0.5 * np.sum([alphas[i] * alphas[j] * y[i] * y[j] * np.dot(x_i, x_j)
                                                for i, x_i in enumerate(X) for j, x_j in enumerate(X)]))
@@ -24,17 +29,19 @@ class MySVC(BaseEstimator):
                 {"type": "ineq", "fun": lambda alphas: alphas}]
 
     def fit(self, X, y):
+        y = self._isinstance_check(y).copy()
+        X = self._isinstance_check(X)
         y[y == 0] = -1
         init_alphas = np.zeros((X.shape[0]))
         self._lagrange_f = partial(self._lagrange_func, X, y)
         self._info = minimize(self._lagrange_f, init_alphas, constraints=self._cons(y))
-        alphas = self._info['x']
-        index_sv = np.where(alphas > 1e-04)[0]
-        self.support_vectors = X[index_sv, :]
-        self.w = (y.reshape(-1, 1) * alphas.reshape(-1, 1) * X).sum(0)
-        self.b = (y[index_sv] - (self.w * self.support_vectors).sum(1)).mean()
+        self._alphas = self._info['x']
+        self._index_sv = np.where(self._alphas > 0.)[0]
+        self.support_vectors = X[self._index_sv, :]
+        self.w = (y.reshape(-1, 1) * self._alphas.reshape(-1, 1) * X).sum(0)
+        self.b = ((self.w * self.support_vectors).sum(1) - y[self._index_sv]).mean()
 
     def predict(self, X):
         pred = np.sign((self.w * X).sum(1) - self.b)
-        pred[np.isin(pred, (-1, 0))] = 0
+        pred[np.isin(pred, -1)] = 0
         return pred
