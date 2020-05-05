fix: GBM mistakes in exploding gradient

gbm.py
@@ -1,21 +1,25 @@
 import numpy as np
-
 from tqdm import tqdm
+from functools import partial
 from sklearn.base import BaseEstimator
 from sklearn.metrics import log_loss, mean_squared_error
 from decision_tree import DecisionTree
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor
+
 
 class GradientBoostingCustom(BaseEstimator):
-    def __init__(self, loss, n_estimators=10, learning_rate=1e-2, max_depth=3, debug=False, random_state=17):
+    def __init__(self,  loss, use_custom_tree=False, n_estimators=10, learning_rate=1e-1, max_depth=3):
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
         self.max_depth = max_depth
-        self.random_state = random_state
-        self.debug = debug
         self.trees_ = []
         self.loss_by_iter_ = []
         self.residuals_by_iter_ = []
+
+        if use_custom_tree:
+            self.tree_class = partial(DecisionTree, criterion='variance', max_depth=self.max_depth)
+        else:
+            self.tree_class = partial(DecisionTreeRegressor, max_depth=self.max_depth)
 
         if loss == 'mse':
             self.loss = mean_squared_error
@@ -33,22 +37,17 @@ class GradientBoostingCustom(BaseEstimator):
             self.loss = self.poisson
             self.grad = self.grad_possion
 
-    def limit_p(self, p):
-        p[p < 10e-5] = 10e-5
-        p[p >= 1.] = 0.999999
-        return p
-
     def sigma(self, z):
         z[z > 100] = 100
         z[z < -100] = -100
         return 1 / (1 + np.exp(-z))
 
     def log_loss(self, y, p):
-        p = self.limit_p(p)
         return log_loss(y, p)
 
     def log_loss_grad(self, y, p):
-        p = self.limit_p(p)
+        p[p == 0.] = 10e-5
+        p[p == 1.] = 1 - 10e-5
         return (p - y) / (p * (1 - p))
 
     def mse_grad(self, y, p):
@@ -68,7 +67,7 @@ class GradientBoostingCustom(BaseEstimator):
         return - y / ((1 + p) * y.size)
 
     def fit(self, X, y):
-        est = DecisionTreeRegressor(max_depth=self.max_depth)
+        est = self.tree_class()
         est.fit(X, y)
         y_t = est.predict(X)
 
@@ -77,22 +76,21 @@ class GradientBoostingCustom(BaseEstimator):
             loss_t = self.loss(y, y_t)
             self.loss_by_iter_.append(loss_t)
             residuals_t = - self.grad(y, y_t)
-            est = DecisionTreeRegressor(max_depth=self.max_depth)
+            est = self.tree_class()
             est.fit(X, residuals_t)
             h_t = est.predict(X)
             y_t += self.learning_rate * h_t
-        print(self.loss_by_iter_)
         return self
 
     def predict_proba(self, X):
         y_pred = self.trees_[0].predict(X)
-        for tree in self.trees_[1:]:
+        for tree in tqdm(self.trees_[1:]):
             y_pred += tree.predict(X)
         return self.sigma(y_pred)
 
     def predict(self, X):
         y_pred = self.trees_[0].predict(X)
-        for tree in self.trees_[1:]:
+        for tree in tqdm(self.trees_[1:]):
             y_pred += tree.predict(X)
         if self.loss in [self.log_loss]:
             return (self.sigma(y_pred) > 0.5).astype(int)